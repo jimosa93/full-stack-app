feac7b3fd23cd2f4e75f39ccabd8972329c6a578
Jorge Morales <jimosa93@gmail.com>
2020-07-22 22:59:45 -0500

    Add redux-thunk

--- frontend/src/actions/todosActions.js
+++ frontend/src/actions/todosActions.js
@@ -1,4 +1,13 @@
-export const listTodos = payload => ({
-  type: 'ADD_TODOS',
-  payload
-});
+import axios from 'axios';
+
+export const listTodos = () => async dispatch => {
+  try {
+    dispatch({ type: 'LOAD_DATA_REQUEST' });
+    const { data } = await axios.get(
+      'https://jsonplaceholder.typicode.com/todos?_limit=10'
+    );
+    dispatch({ type: 'LOAD_DATA_SUCCESS', payload: data });
+  } catch (error) {
+    dispatch({ type: 'LOAD_DATA_FAILURE', payload: error.message });
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,47 +1,15 @@
-import { compose } from 'redux';
-import { createStore } from 'redux';
+import { compose, applyMiddleware } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import todosReducers from './reducers/todosReducers';
-
-const initialState = {
-  todos: [
-    {
-      userId: 1,
-      id: 1,
-      title: 'delectus aut autem',
-      completed: false
-    },
-    {
-      userId: 1,
-      id: 2,
-      title: 'quis ut nam facilis et officia qui',
-      completed: false
-    },
-    {
-      userId: 1,
-      id: 3,
-      title: 'fugiat veniam minus',
-      completed: false
-    },
-    {
-      userId: 1,
-      id: 4,
-      title: 'et porro tempora',
-      completed: true
-    },
-    {
-      userId: 1,
-      id: 5,
-      title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
-      completed: false
-    }
-  ]
-};
 
 const composeEnhancers =
   (typeof window !== 'undefined' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
-const store = createStore(todosReducers, initialState, composeEnhancers());
+const reducers = combineReducers({ todos: todosReducers });
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
